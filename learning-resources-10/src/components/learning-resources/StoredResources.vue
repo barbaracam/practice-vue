44ab0353ef03221d765a2b447f9952b841a94277
Barbara Cam <cam.barbara@hotmail.com>
2023-04-13 01:23:30 -0400

    chapter 10 - part1

---
+++ learning-resources-10/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    data(){
+    return{}
+    }
+    }
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
