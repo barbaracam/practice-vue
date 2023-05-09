2d00360c11e86ff994e3522bbe99a7cc1dbc8ca6
Barbara Cam <cam.barbara@hotmail.com>
2023-05-09 17:56:02 -0400

    migration chap 19

--- chap19-migrating-part2app/src/components/projects/ProjectsList.vue
+++ chap19-migrating-part2app/src/components/projects/ProjectsList.vue
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import {ref, computed, watch, toRefs} from "vue"
+
 import ProjectItem from './ProjectItem.vue';
 
 export default {
@@ -20,41 +22,101 @@ export default {
     ProjectItem,
   },
   props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
+  setup(props){
+      const enteredSearchTerm = ref('');
+      const activeSearchTerm =  ref('');
+
+      const hasProjects = computed(function(){
+        return props.user.projects && availableProjects.value.length > 0;
+      });
+
+      const availableProjects = computed(function() {
+         if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
         );
       }
-      return this.user.projects;
-    },
+      return props.user.projects;
+      })
+      //watch 
+      watch(enteredSearchTerm, function(newValue){
+        setTimeout(() => {
+          if (newValue === enteredSearchTerm.value) {
+
+            //activeSearchTerm below is not a dependency, because changes is an activeSearchTerm should not trigger the watcher
+            activeSearchTerm.value = newValue;
+          }
+        }, 300);
+      })
+
+      //wont work as we drilling into a reactive object to then watch a no reactive value
+      //props is a reactive value, value inside props are not reactive are not ref
+      //this work because we only have one props, but in case we have more, watcher will work with any changes during the props
+      //we will use torefs and extract the user
+
+      // const propsWithRefs = toRefs(props);
+      // const user = propsWithRefs.user; //or with object restructuring
+
+      const { user } = toRefs(props);
+
+      // watch(props.user, function(){
+        watch(user, function(){
+        enteredSearchTerm.value = '';
+      })
+
+      //we reset the enteredSearchTerm, whenever we have a new user
+
+      //methods
+      function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+
+    //return
+
+    return {
+      //we dont call user, as it is call during the props
+      hasProjects,
+      updateSearch,
+      enteredSearchTerm,
+      availableProjects,
+
+    }
+  },
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //   };
+  // },
+  computed: {
+    // hasProjects() {
+    //   return this.user.projects && this.availableProjects.length > 0;
+    // },
+    // availableProjects() {
+    //   if (this.activeSearchTerm) {
+    //     return this.user.projects.filter((prj) =>
+    //       prj.title.includes(this.activeSearchTerm)
+    //     );
+    //   }
+    //   return this.user.projects;
+    // },
   },
   methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
+    // updateSearch(val) {
+    //   this.enteredSearchTerm = val;
+    // },
   },
   watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    // enteredSearchTerm(val) {
+    //   setTimeout(() => {
+    //     if (val === this.enteredSearchTerm) {
+    //       this.activeSearchTerm = val;
+    //     }
+    //   }, 300);
+    // },
+    // user() {
+    //   this.enteredSearchTerm = '';
+    // },
   },
 };
 </script>
