5b67ce2a39e464b97ae2bdc24cd05867587b6bae
Barbara Cam <cam.barbara@hotmail.com>
2023-05-09 22:23:07 -0400

    vuex done chap 19

---
+++ chap19-vuex/src/components/ControlCenter.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script>
+import {useStore} from 'vuex'
+
+export default {
+  setup() {
+    //instead of $store, we will use useStore
+    const store = useStore();
+    function inc() {
+        store.dispatch('increment');
+    }
+
+    return { inc };
+  },
+};
+</script>
---
+++ chap19-vuex/src/components/TheCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>{{ counter }}</h2>
+</template>
+
+<script>
+import { computed } from 'vue';
+import {useStore} from 'vuex';
+
+//get a counter than depend of a getter, we need to use computed variable, value
+export default {
+  setup() {
+    const store = useStore();
+
+    const counter = computed(function(){
+        return store.getters.counter
+    });
+
+    return { counter };
+  },
+};
+</script>
