475b2b82a3d2aaf09949dc9f473807a3c94ac29d
Barbara Cam <cam.barbara@hotmail.com>
2023-05-09 22:01:26 -0400

    routing chap 19 done

---
+++ chap19-routing/src/pages/ProductDetails.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <h3>${{ price}}</h3>
+    <p>{{ description}}</p>
+    <router-link to="/products/p2">Product 2</router-link>
+  </section>
+</template>
+
+<script>
+import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default {
+  props:['pid'],
+  // setup(props) {
+    setup() {
+
+    const products = inject('products');
+
+    const route = useRoute();
+    console.log(route);
+
+    //we dont need the refs and we dont want to change the data only accessed and we can do it with the const of select product and injection as we dont need a modification
+    // const title = ref('');
+    // const price = ref(null);
+    // const description = ref('');
+
+  //accesing products from the injection
+    // const selectedProduct = computed(() => products.value.find(product => product.id === props.pid));
+    const selectedProduct = computed(() => products.value.find(product => product.id === route.params.pid));
+    const title = computed(() => selectedProduct.value.title);
+    const price = computed(() => selectedProduct.value.price);
+    const description = computed(() => selectedProduct.value.description);
+
+    
+    return { title, price, description };
+  },
+};
+</script>
+
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
